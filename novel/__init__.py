f126ec3d433654cd24e2c2373b6fdc6ead07297d
Gandi-95 <609531039@qq.com>
2019-10-17 00:47:24 +0800

    添加源

--- novel/__init__.py
+++ novel/__init__.py
@@ -3,6 +3,7 @@ import time
 
 if __name__ == '__main__':
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    novelName = input('请输入书名:')
-    novelsource = novelSource.biquge5200()
-    novelsource.init(novelName)
+    # novelName = input('请输入书名:')
+    novelsource = novelSource.biqugex()
+    # novelsource.init(novelName)
+    novelsource.init('大清')
--- novel/novelSource.py
+++ novel/novelSource.py
@@ -2,16 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import downloadNovel
 
+import urllib.parse
+import sys
+
 
 class source:
-
     # 定义网站base_url和搜索url
     def __init__(self):
         self.host_url = ""
         self.seach_url = ''
 
+
+
     # 获取网站的内容
-    def get_Html(self,url):
+    def get_Html(self,url,encoding='utf-8'):
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Accept - Encoding': 'gzip, deflate, br',
@@ -20,16 +24,20 @@ class source:
             'Connection': 'keep-alive',
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-        html_doc = requests.get(url, headers=headers).text
-        soup = BeautifulSoup(html_doc, 'html.parser')
+        html_doc = requests.get(url, headers=headers)
+        html_doc.encoding = encoding
+        soup = BeautifulSoup(html_doc.text, 'html.parser')
         return soup
 
+    def seachUrl(self, novelName):
+        return self.seach_url.format(novelName)
+
     # 获取搜索到的小说
-    def getNovels(self,novels_soup):
+    def getNovels(self, novels_soup):
         print()
 
     # 显示搜索到的小说
-    def showSearchNovel(self,novels):
+    def showSearchNovel(self, novels):
         if novels != None:
             index = 1
             print('搜索结果:')
@@ -39,7 +47,7 @@ class source:
                 print(a)
 
     # 选择搜索到的小说
-    def selectNovel(self,str,len):
+    def selectNovel(self, str, len):
         index = input(str)
         try:
             index = int(index)
@@ -50,39 +58,28 @@ class source:
         return index
 
     # 获取选择小说的目录，返回{title:herf}的列表
-    def getCatalog(self,novel):
+    def getCatalog(self, novel):
         print()
 
-
-    def init(self,novelName):
-        print(self.seach_url.format(novelName))
-        novels_soup = self.get_Html(self.seach_url.format(novelName))
+    def init(self, novelName):
+        url = self.seachUrl(novelName)
+        print(url)
+        novels_soup = self.get_Html(url)
         novels = self.getNovels(novels_soup)
         self.showSearchNovel(novels)
-        index = self.selectNovel('请输入需要下载的序号:',len(novels))
+        index = self.selectNovel('请输入需要下载的序号:', len(novels))
         catalog_list = self.getCatalog(novels[index - 1])
-        for catalog in catalog_list:
-            title = '    ' + list(catalog.keys())[0]
-            print(title)
-        downloadNovel.start(catalog_list,novelName)
+        downloadNovel.start(catalog_list, novelName)
 
 
-
-
-
-
-
-# ——————————————————————————笔趣阁5200————————————————————————————
-
-class biquge5200(source):
-
-    def __init__(self):
-        self.host_url = "https://www.biquge5200.cc/"
-        self.seach_url = 'https://www.biquge5200.cc/modules/article/search.php?searchkey={}'
+# ——————————————————————————笔趣阁系列————————————————————————————
+class biquge(source):
+    def findAllNovels(self):
+        return 'td', 'odd'
 
     def getNovels(self, novels_soup):
         seach_result_list = []
-        td = novels_soup.find_all('td', 'odd')
+        td = novels_soup.find_all(self.findAllNovels())
         for item in td:
             a = item.a
             if a != None:
@@ -92,16 +89,23 @@ class biquge5200(source):
         return seach_result_list
 
 
-    def getCatalog(self,novel):
-        catalog_soup = self.get_Html(list(novel.values())[0])
+    def getCatalogHtml(self,url):
+        return self.get_Html(list(url))
+
+    def getCatalogInfo(self, a):
+        title = a.get_text()
+        href = a.get('href')
+        print(title + href)
+        return {title, href}
+
+    def getCatalog(self, novel):
+        catalog_soup = self.getCatalogHtml(list(novel.values())[0])
         catalog = catalog_soup.find_all('dd')
         catalog_list = []
         for item in catalog:
             a = item.a
             if a != None:
-                title = a.get_text()
-                href = a.get('href')
-                catalog_list.append({title: href})
+                catalog_list.append(self.getCatalogInfo(a))
 
         # 前面9章为最新更新的章节，所以过滤
         if catalog_list != None:
@@ -112,3 +116,35 @@ class biquge5200(source):
                 catalog_list = catalog_list[9:lenght]
 
         return catalog_list
+
+
+# ——————————————https://www.biquge5200.cc/————————————————————————————————
+class biquge5200(biquge):
+    def __init__(self):
+        self.host_url = "https://www.biquge5200.cc"
+        self.seach_url = 'https://www.biquge5200.cc/modules/article/search.php?searchkey={}'
+
+    def findAllNovels(self):
+        return 'td', 'odd'
+
+
+# ————————————————https://www.biqugex.com/————————————————————————————————
+class biqugex(biquge):
+    def __init__(self):
+        self.host_url ='https://www.biqugex.com'
+        self.seach_url = 'https://so.biqusoso.com/s.php?ie=gbk&siteid=biqugex.com&s=9157106854577873494&q={}'
+
+    def seachUrl(self, novelName):
+        return self.seach_url.format(urllib.parse.quote(novelName.encode('gbk')))
+
+    def findAllNovels(self):
+        return 'li'
+
+    def getCatalogHtml(self,url):
+        return self.get_Html(url,'gb18030')
+
+    def getCatalogInfo(self, a):
+        title = a.get_text()
+        href = self.host_url+a.get('href')
+        print(title + href)
+        return {title, href}
